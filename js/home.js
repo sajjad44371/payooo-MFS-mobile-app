2277949fecbfece39c80aaf2f922aa311270d95e
Sajjad Hossain <sajjadhossain44371@gmail.com>
2025-09-10 20:49:02 +0600

    added all section without transaction

--- js/home.js
+++ js/home.js
@@ -3,15 +3,32 @@ document.getElementById("btn-logout").addEventListener("click", function () {
   window.location.href = "./index.html";
 });
 
-// add event listener
 const validPin = 1234;
 
+// add
 const add = document.getElementById("add");
 add.addEventListener("click", function () {
   const allFormSection = document.querySelectorAll(".form-section");
   for (const el of allFormSection) {
     el.style.display = "none";
     document.getElementById("add-sec").style.display = "block";
+
+    const allCard = document.querySelectorAll(".card");
+    for (const card of allCard) {
+      console.log(card);
+      card.classList.remove(
+        "bg-[#0874f20d]",
+        "border-[#0874F2]",
+        "text-[#0874F2]"
+      );
+      card.classList.add("bg-white", "border-gray-300");
+    }
+    document
+      .getElementById("add")
+      .classList.remove("bg-white", "border-gray-300");
+    document
+      .getElementById("add")
+      .classList.add("bg-[#0874f20d]", "border-[#0874F2]", "text-[#0874F2]");
   }
 
   const addMoneyButton = document.getElementById("btn-add");
@@ -64,6 +81,23 @@ cashOut.addEventListener("click", function () {
   for (const el of allFormSection) {
     el.style.display = "none";
     document.getElementById("cashout-sec").style.display = "block";
+
+    const allCard = document.querySelectorAll(".card");
+    for (const card of allCard) {
+      console.log(card);
+      card.classList.remove(
+        "bg-[#0874f20d]",
+        "border-[#0874F2]",
+        "text-[#0874F2]"
+      );
+      card.classList.add("bg-white", "border-gray-300");
+    }
+    document
+      .getElementById("cashout")
+      .classList.remove("bg-white", "border-gray-300");
+    document
+      .getElementById("cashout")
+      .classList.add("bg-[#0874f20d]", "border-[#0874F2]", "text-[#0874F2]");
   }
 
   const withdrawBtn = document.getElementById("btn-withdraw");
@@ -111,6 +145,23 @@ transfer.addEventListener("click", function () {
   for (const el of allFormSection) {
     el.style.display = "none";
     document.getElementById("transfer-sec").style.display = "block";
+
+    const allCard = document.querySelectorAll(".card");
+    for (const card of allCard) {
+      console.log(card);
+      card.classList.remove(
+        "bg-[#0874f20d]",
+        "border-[#0874F2]",
+        "text-[#0874F2]"
+      );
+      card.classList.add("bg-white", "border-gray-300");
+    }
+    document
+      .getElementById("transfer")
+      .classList.remove("bg-white", "border-gray-300");
+    document
+      .getElementById("transfer")
+      .classList.add("bg-[#0874f20d]", "border-[#0874F2]", "text-[#0874F2]");
   }
 
   const sendBtn = document.getElementById("btn-send");
@@ -148,5 +199,120 @@ transfer.addEventListener("click", function () {
     document.getElementById("transfer-acc").value = "";
     document.getElementById("transfer-amount").value = "";
     document.getElementById("transfer-pin").value = "";
+  });
+});
+
+// get bonus
+const getBonus = document.getElementById("bonus");
+getBonus.addEventListener("click", function () {
+  const allFormSection = document.querySelectorAll(".form-section");
+  for (const el of allFormSection) {
+    el.style.display = "none";
+    document.getElementById("bonus-sec").style.display = "block";
+
+    const allCard = document.querySelectorAll(".card");
+    for (const card of allCard) {
+      console.log(card);
+      card.classList.remove(
+        "bg-[#0874f20d]",
+        "border-[#0874F2]",
+        "text-[#0874F2]"
+      );
+      card.classList.add("bg-white", "border-gray-300");
+    }
+    document
+      .getElementById("bonus")
+      .classList.remove("bg-white", "border-gray-300");
+    document
+      .getElementById("bonus")
+      .classList.add("bg-[#0874f20d]", "border-[#0874F2]", "text-[#0874F2]");
+  }
+
+  const bonusBtn = document.getElementById("btn-bonus");
+  bonusBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const couponNumber = parseInt(document.getElementById("coupon-num").value);
+    const mainAmount = parseInt(
+      document.getElementById("main-amount").innerText
+    );
+
+    // validation
+    if (isNaN(couponNumber)) {
+      alert("Please enter valid coupon number.");
+      return;
+    } else {
+      alert("Congratulations! You won 5% bonus");
+      const finalAmount = mainAmount + (mainAmount * 5) / 100;
+      document.getElementById("main-amount").innerText = finalAmount;
+    }
+
+    document.getElementById("coupon-num").value = "";
+  });
+});
+
+// pay bill
+const payBill = document.getElementById("pay");
+payBill.addEventListener("click", function () {
+  const allFormSection = document.querySelectorAll(".form-section");
+  for (const el of allFormSection) {
+    el.style.display = "none";
+    document.getElementById("pay-sec").style.display = "block";
+
+    const allCard = document.querySelectorAll(".card");
+    for (const card of allCard) {
+      console.log(card);
+      card.classList.remove(
+        "bg-[#0874f20d]",
+        "border-[#0874F2]",
+        "text-[#0874F2]"
+      );
+      card.classList.add("bg-white", "border-gray-300");
+    }
+    document
+      .getElementById("pay")
+      .classList.remove("bg-white", "border-gray-300");
+    document
+      .getElementById("pay")
+      .classList.add("bg-[#0874f20d]", "border-[#0874F2]", "text-[#0874F2]");
+  }
+
+  const payBtn = document.getElementById("btn-pay");
+  payBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const paybank = document.getElementById("pay-bank").value;
+    const billerAccNum = document.getElementById("biller-acc").value;
+    const payPin = parseInt(document.getElementById("pay-pin").value);
+    const payAmount = parseInt(document.getElementById("pay-amount").value);
+    const mainAmount = parseInt(
+      document.getElementById("main-amount").innerText
+    );
+
+    // validation
+    if (paybank === "Select bank") {
+      alert("Please select a bank.");
+      return;
+    } else if (billerAccNum.length < 11) {
+      alert("Please enter valid biller account.");
+      return;
+    } else if (isNaN(payAmount)) {
+      alert("Please enter your amount.");
+      return;
+    } else if (payPin !== validPin) {
+      alert("Please enter correct pin number.");
+      return;
+    }
+
+    if (mainAmount < payAmount) {
+      alert("You haven't enough money to pay bill");
+    } else {
+      const finalAmount = mainAmount - payAmount;
+      document.getElementById("main-amount").innerText = finalAmount;
+    }
+
+    document.getElementById("biller-acc").value = "";
+    document.getElementById("pay-amount").value = "";
+    document.getElementById("pay-pin").value = "";
   });
 });
